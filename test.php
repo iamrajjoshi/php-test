a235825313e37c178867963b782c97a7411acd81
Raj Joshi <raj.joshi@sentry.io>
2024-03-04 18:14:27 -0800

    test pr

---
+++ test.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'vendor/autoload.php'; // Include Composer's autoload
+
+\Sentry\init([
+    'dsn' => 'http://1937d96adff3ad76db240aedc9ad9194@dev.getsentry.net:8000/8',
+    // Specify a fixed sample rate
+    'traces_sample_rate' => 1.0,
+  ]);   
+class Fruit {
+  // Properties
+  public $name;
+  public $color;
+
+  // Methods
+  function set_name($name) {
+    $this->names = $name;
+  }
+  function get_name() {
+    return $this->name;
+  }
+}
+
+$apple = new Fruit();
+$banana = new Fruit();
+$apple->set_name('Apple');
+$banana->set_name('Banana');
+
+echo $apple->get_name();
+echo "<br>";
+echo $banana->get_name();
+echo $banana->get_name();
+?>
