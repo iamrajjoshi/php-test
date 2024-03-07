a0f8b3e30187ba85728f6f734d31edb743338f51
Raj Joshi <raj.joshi@sentry.io>
2024-03-07 13:35:58 -0800

    changes

--- a.py
+++ a.py
@@ -1,9 +1,20 @@
 # take file, split on each line, de-duplicate, and print as a list, remove the newline character
-def function_name_here():
-    with open("pagination_error.txt") as f:
-        lines = f.readlines()
-        lines = list(set(lines))
-        lines = [line.strip() for line in lines]
-        print(len(lines))
 
-function_name_here()
+class A:
+    def test(self):
+        pass
+
+    def test2(self):
+        pass
+
+    def function_name_here():
+        with open("pagination_error.txt") as f:
+            lines = f.readlines()
+            lines = list(set(lines))
+            lines = [line.strip() for line in lines]
+            print(len(lines))
+
+
+
+a = A()
+a.function_name_here()
